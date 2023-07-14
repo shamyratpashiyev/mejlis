64313ab8f46421390b5fe17fe643f3cd088c36a0
shamurat <it.shamurat@st.com.tm>
2023-07-14 14:04:38 +0500

    Added popup windows to mejlis-deputies page

---
+++ resources/views/components/election-district-popup.blade.php
@@ -0,0 +1,10 @@
+<div class="district_popup_container flex_row">
+
+    <div class="popup_content_block flex_column">
+        <span class="district_name">{{ $name }}</span>
+        <svg class="remove_button" width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.7426 8.36236L8.25732 16.8476M16.7426 16.8476L8.25732 8.3623" stroke="#28303F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <p>{!! $description !!}</p>
+    </div>
+</div>
--- resources/views/mejlis_deputies.blade.php
+++ resources/views/mejlis_deputies.blade.php
@@ -8,7 +8,18 @@
     $links_list = [
                     ['name'=>'Mejlis komitetleri ', 'url' => '#'],
                     ['name'=>'Mejlis deputatlary ','url' => '#'], 
-                ]
+];
+    $deputy_description = "<p>
+            Kasymguly Gulmyradowiç Babaýew 1966-njy ýylyň 12-nji sentýabrynda Ahal welaýatynyň Ak bugdaý etrabynyň Gämi obasynda doguldy. Milleti türkmen.
+            1973 — 1983-nji ýyllarda Ahal welaýaty- nyň Ak bugdaý etrabyndaky l-nji orta mek- depde okady. 1983 — 1989-njy ýyllarda Türkmen döwlet ykdysadyýet we dolandy- ryş institutynda okap, ony ykdysatçy hünäri boýunça tamamlady. 1984 — 1986-njy ýyl- larda harby gullukda boldy. 1989 — 1991- nji ýyllarda SSSR-iň Zähmet we durmuş üpjünçiligi baradaky döw- let komitetiniň Zähmet ylmy-barlag institutynyň Türkmenistandaky bölüminde ykdysatçy bolup işledi. 1991 — 1993-nji ýyllarda Türk­men döwlet ykdysadyýet we dolandyryş institutynyň dolandyryş we halkara ykdysady aragatnaşyklar kafedrasynyň mugallymy bolup işledi. 1993 — 1996-njy ýyllarda şol institutyň dünýä ykdysadyýe- ti we halkara ykdysady gatnaşyklar kafedrasynyň mugallymy, şol bir wagtda, 1991-nji ýylyň sentýabr aýynda institutyň kommersiýa- tehnologiýa fakultetiniň dekanynyň orunbasary wezipesine belle­nildi we 1992-nji ýylyň sentýabr aýynda söwda we halkara ykdysa- dy aragatnaşyklar fakultetiniň dekanynyň orunbasary bolup işledi. 1993 — 1995-nji ýyllarda aspiranturada okady. 1996 — 2002-nji ýyllarda Magtymguly adyndaky Türkmen döwlet uniwersitetiniň dünýä ykdysadyýeti we halkara ykdysady gatnaşyklar kafedrasy- nyň mugallymy, 2002 — 2007-nji ýyllarda şol uniwersitetiň dünýä tejribesini öwreniş kafedrasynyň uly mugallymy bolup işledi. Şol bir wagtda, 2000-nji ýylyň ýanwar aýyndan başlap uniwersitetiň hukuk we halkara gatnaşyklary fakultetiniň dekanynyň orunbasary wezipesine bellendi we şol ýylyň sentýabr aýyndan başlap, 2007- nji ýylyň fewral aýyna çenli hukuk we halkara gatnaşyklar fakulteti- niň dekany wezipesinde işledi.
+            2007 — 2012-nji ýyllarda Türkmenistanyň Mejlisiniň Başlygy- nyň orunbasary wezipesinde işledi. 2008-nji ýylyň iýun aýyndan şol bir wagtda Türkmenistanyň Umumymilli «Galkynyş» hereke- tiniň başlygynyň birinji orunbasary, Türkmenistanyň Demokratik partiýasynyň Syýasy Geňeşiniň birinji sekretary, 2013 — 2018-nji ýyllarda Türkmenistanyň Demokratik partiýasynyň başlygy wezi- pelerinde işledi. 2018 — 2021-nji ýyllarda Türkmenistanyň Mejlisi- niň Başlygynyň orunbasary bolup işledi. Türkmenistanyň Mejlisiniň III, IV, V, VI çagyrylyşlarynyň deputaty. 2021 — 2023-nji ýyllarda Türkmenistanyň Milli Geňeşiniň Halk Maslahatynyň Başlygynyň orunbasary bolup işledi. Häzirki wagtda Türkmenistanyň Halk Maslahatynyň Diwanynyň Iş dolandyryjysy wezipesinde işleýär.
+            2010-njy ýylda Türkmenistanyň «Watana bolan söýgüsi üçin» medaly, 2011-nji ýylda «Türkmenistanyň Garaşsyzlygynyň 20 ýyl- lygyna» atly ýubileý medaly, 2014-nji ýylda Türkmenistanyň «Mag- tymguly Pyragy» medaly, 2015-nji ýylda Türkmenistanyň «Garaş- syz, Baky Bitarap Türkmenistan» ýubileý medaly, 2016-njy ýylda «Türkmenistanyň Garaşsyzlygynyň 25 ýyllygyna» atly Türkmenis- tanyň ýubileý medaly, 2017-nji ýylda Türkmenistanyň Garaşsyzly- gynyň 26 ýyllygy mynasybetli ýadygärlik nyşany bilen sylaglanyldy.
+            Maşgalaly, dört çagasy bar.
+            Türkmenistanyň Demokratik partiýasy tarapyndan dalaşgär- lige hödürlenildi.
+        </p>";
+    
+    $district_desc = "<p>Berkararlyk etrabynyň Gurbansoltan eje şaýolunyň S.Türkmenbaşy şaýoly bilen kesişýän çatrygyndan başlap, Gurbansoltan eje şaýolunyň ugry bilen B.Annanow köçesine çenli, şol ýerden B.Annanow köçesiniň ugry bilen A.Nyýazow şaýoluna çenli, A.Nyýazow şaýolunyň ugry bilen A.Garlyýew köçesine çenli, A.Garlyýew köçesiniň ugry bilen Galkynyş köçesine çenli, Galkynyş köçesiniň ugry bilen S.Türkmenbaşy şaýoluna çenli, S.Türkmenbaşy şaýolunyň ugry bilen Gurbansoltan eje şaýoluna çenli araçäkleri öz içine alýar</p>";
+
 @endphp
 
 @section('content')
@@ -75,12 +86,14 @@
 
                     <div class="deputies_block flex_row">
 
-                        <div class="deputy_container flex_column">
-                            <a href="{{ route('single_deputy_page',1) }}" class="name">Babaýew Kasymguly Gulmyradowiç</a>
+                        <div class="deputy_container flex_column" data-href-value="#">
+                            <span class="name">Babaýew Kasymguly Gulmyradowiç</span>
+                            <x-deputy-popup name="Babaýew Kasymguly Gulmyradowiç" district="1-nji «Garaşsyzlyk» saýlaw okrugy" :description="$deputy_description" />
 
-                            <div class="key_value_wrapper flex_column">
-                                <span class="key">Saýlaw okrugy:</span>
+                            <div class="key_value_wrapper flex_column" data-district-id='1'>
+                                <span class="key election_district">Saýlaw okrugy:</span>
                                 <span class="value">1-nji «Garaşsyzlyk» saýlaw okrugy</span>
+                                <x-election-district-popup name="1-nji «Garaşsyzlyk» saýlaw okrugy" :description="$district_desc"/>
                             </div>
 
                             <div class="key_value_wrapper flex_column">
@@ -94,11 +107,11 @@
                             </div>
                         </div>
 
-                        <div class="deputy_container flex_column">
-                            <a href="#" class="name">Muhamedow Begmurat Rahmangulyýewiç</a>
+                        <div class="deputy_container flex_column" data-href-value="#">
+                            <span class="name">Muhamedow Begmurat Rahmangulyýewiç</span>
 
                             <div class="key_value_wrapper flex_column">
-                                <span class="key">Saýlaw okrugy:</span>
+                                <span class="key election_district">Saýlaw okrugy:</span>
                                 <span class="value">2-nji «Bitaraplyk» saýlaw okrugy</span>
                             </div>
 
@@ -113,11 +126,11 @@
                             </div>
                         </div>
 
-                        <div class="deputy_container flex_column">
-                            <a href="#" class="name">Öwekowa Jenet Ýazberdiýewna</a>
+                        <div class="deputy_container flex_column" data-href-value="#">
+                            <span class="name">Öwekowa Jenet Ýazberdiýewna</span>
 
                             <div class="key_value_wrapper flex_column">
-                                <span class="key">Saýlaw okrugy:</span>
+                                <span class="key election_district">Saýlaw okrugy:</span>
                                 <span class="value">3-nji «Azatlyk» saýlaw okrugy</span>
                             </div>
 
@@ -132,11 +145,11 @@
                             </div>
                         </div>
 
-                        <div class="deputy_container flex_column">
-                            <a href="#" class="name">Owganow Saparmyrat Esenowiç</a>
+                        <div class="deputy_container flex_column" data-href-value="#">
+                            <span class="name">Owganow Saparmyrat Esenowiç</span>
 
                             <div class="key_value_wrapper flex_column">
-                                <span class="key">Saýlaw okrugy:</span>
+                                <span class="key election_district">Saýlaw okrugy:</span>
                                 <span class="value">4-nji «Berkararlyk» saýlaw okrugy</span>
                             </div>
 
@@ -151,11 +164,11 @@
                             </div>
                         </div>
 
-                        <div class="deputy_container flex_column">
-                            <a href="#" class="name">Gulmanowa Dünýägözel Akmuhammedowna</a>
+                        <div class="deputy_container flex_column" data-href-value="#">
+                            <span class="name">Gulmanowa Dünýägözel Akmuhammedowna</span>
 
                             <div class="key_value_wrapper flex_column">
-                                <span class="key">Saýlaw okrugy:</span>
+                                <span class="key election_district">Saýlaw okrugy:</span>
                                 <span class="value">5-nji «Parahatçylyk» saýlaw okrugy</span>
                             </div>
 
@@ -170,11 +183,11 @@
                             </div>
                         </div>
 
-                        <div class="deputy_container flex_column">
-                            <a href="#" class="name">Babanyýazow Çarygeldy Gurbangeldiýewiç</a>
+                        <div class="deputy_container flex_column" data-href-value="#">
+                            <span class="name">Babanyýazow Çarygeldy Gurbangeldiýewiç</span>
 
                             <div class="key_value_wrapper flex_column">
-                                <span class="key">Saýlaw okrugy:</span>
+                                <span class="key election_district">Saýlaw okrugy:</span>
                                 <span class="value">6-njy «Köpetdag» saýlaw okrugy</span>
                             </div>
 
@@ -194,4 +207,30 @@
             </div>
         </div>
     </div>
+    <script>
+        let href_value = ''
+
+        $('.deputy_container, .deputy_container > .name, .deputy_container .election_district').on('click', (event) => {
+            if($(event.target).hasClass('name')){   //if the deputy_name is clicked
+                $(event.target).siblings('.deputy_popup_container').addClass('active')
+
+            } else if($(event.target).hasClass('election_district')){       //if the election_district is clicked
+                $(event.target).siblings('.district_popup_container').addClass('active')
+
+            } else if($(event.target).parents().hasClass('deputy_popup_container') || $(event.target).parents().hasClass('district_popup_container')){
+
+            } else {
+                href_value = $(event.target).data('href-value')  //if the block itself is the event target
+                ? $(event.target).data('href-value') 
+                : $(event.target).parent().data('href-value') ?? $(event.target).parent().parent().data('href-value') //else if first child or second child is the event target
+                window.location.replace(href_value)
+            }
+            
+        })
+
+        $('.deputy_popup_container .remove_button, .district_popup_container .remove_button').on('click', (event) => {
+            $(event.target).parent().parent().removeClass('active')
+        })
+        
+    </script>
 @endsection
