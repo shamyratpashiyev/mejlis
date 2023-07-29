762814ccf549ff6854100b3fc59e9f5c8e6fab33
shamurat <it.shamurat@st.com.tm>
2023-07-29 10:12:55 +0500

    Modified LocalizationMiddleware

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\Lang;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
@@ -17,7 +18,8 @@ class LocalizationMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(!$request->query('lang')){
+        $active_langs = Lang::where('is_active',true)->get()->pluck('code')->toArray();
+        if(!$request->query('lang') || !in_array($request->query('lang'), $active_langs)){
             return redirect(route('main_page',['lang'=>'tm']));
         }
         App::setLocale($request->query('lang'));
