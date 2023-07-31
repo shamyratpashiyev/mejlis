df870be73d7492bfb9e90f3797ad41472ef5e211
shamurat <it.shamurat@st.com.tm>
2023-07-31 13:42:41 +0500

    Added tkm_constitution_page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         
         $this->call([
             ElectionDistrictSeeder::class,
-            DummyDataSeeder::class,
+            // DummyDataSeeder::class,
             AdminSeeder::class,
         ]);
     }
