4a28ff92f0e277d3c1ff745cba94f130227a704a
shamurat <it.shamurat@st.com.tm>
2023-08-18 11:54:56 +0500

    Uncommented AdminSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             // ElectionDistrictSeeder::class,
             // DummyDataSeeder::class,
-            // AdminSeeder::class,
+            AdminSeeder::class,
         ]);
     }
 }
